d8b7da935820e8f0f19780bdcc88a54fbe9aed6c
Samuel Debionne <samuel.debionne@ujf-grenoble.fr>
2015-03-09 18:20:09 +0100

    [algorithms] Implement runtime error for nyi algo
    Add an implementation of not_implemented_error that either static assert
    or throw a runtime error depending on a policy for a given algorithm.

--- include/boost/geometry/algorithms/not_implemented.hpp
+++ include/boost/geometry/algorithms/not_implemented.hpp
@@ -3,6 +3,7 @@
 // Copyright (c) 2008-2015 Bruno Lalande, Paris, France.
 // Copyright (c) 2007-2015 Barend Gehrels, Amsterdam, the Netherlands.
 // Copyright (c) 2009-2015 Mateusz Loskot, London, UK.
+// Copyright (c) 2015 Samuel Debionne, Grenoble, France.
 
 // This file was modified by Oracle on 2015.
 // Modifications copyright (c) 2015, Oracle and/or its affiliates.
@@ -21,8 +22,16 @@
 #define BOOST_GEOMETRY_ALGORITHMS_NOT_IMPLEMENTED_HPP
 
 
+#include <exception>
+#include <string>
+
 #include <boost/mpl/assert.hpp>
+#include <boost/mpl/bool.hpp>
+#include <boost/mpl/if.hpp>
 #include <boost/mpl/identity.hpp>
+
+#include <boost/type_traits/is_base_of.hpp>
+
 #include <boost/geometry/core/tags.hpp>
 
 
@@ -47,25 +56,134 @@ namespace info
 }
 
 
-namespace nyi
-{
+/// A base class for every algorithm tags
+/// Derived class must implement as static function
+/// const char* what();
+/// that return a textual information on the algorithm.
+///
+/// Example :
+///
+/// struct within_tag : algorithm_tag
+/// {
+///     static const char* what() { return "within"; }
+/// };
+struct algorithm_tag {};
 
+/// Exception class thrown when a functionnality is not implemented
+/// for the given argument types
+struct not_implemented_runtime_error : std::runtime_error
+{
+    explicit not_implemented_runtime_error(const std::string& what)
+        : std::runtime_error(what) {}
+    explicit not_implemented_runtime_error(const char* what)
+        : std::runtime_error(what) {}
+};
+
+
+#ifndef BOOST_GEOMETRY_IMPLEMENTATION_STATUS_BUILD
+# define BOOST_GEOMETRY_IMPLEMENTATION_STATUS_BUILD 0
+#endif
+
+/// Use this policy or BOOST_GEOMETRY_NYI_RUNTIME_ERROR to
+/// specify whether the algorithm should report invalid parameter
+/// type at compile (false) or runtime (true)
+template <class T>
+struct not_implemented_policy :
+#if BOOST_GEOMETRY_IMPLEMENTATION_STATUS_BUILD
+    boost::mpl::true_
+#else
+    boost::mpl::false_
+#endif
+{};
+
+
+namespace nyi {
 
 struct not_implemented_tag {};
 
+
+/// A fake result type used to 'implement' archetypes of not implemented
+/// algorithm / strategies...
+struct not_implemented_result
+{
+    //TODO add support for compilers that do not support variadic template
+    template <typename... Args>
+    not_implemented_result(Args...) {}
+
+    template <typename T>
+    operator T()
+    {
+        // This will never actually happen - we just need to write something
+        // that hopefully all compilers are happy with (ideally without warnings)
+        // and doesn't assume anything about T
+        BOOST_ASSERT(false);
+        
+        T* t = NULL;
+        return *t;
+    }
+};
+
+
+/// Archetype of a result_handler
+struct not_implemented_result_handler
+{
+    typedef not_implemented_result result_type;
+
+    //TODO add support for compilers that do not support variadic template
+    template <typename... Args>
+    not_implemented_result_handler(Args...) {}
+
+    not_implemented_result handle() const
+    {
+        return not_implemented_result();
+    }
+};
+
+/// Archetype of an algorithm / strategy
+/// The Tag template parameter enables to specify the runtime / compile time
+/// policy on a per alogorithm basis and gives information on the not implemented
+/// algorithms.
+/// 
+/// Compile time : sqtatic assert (default)
+/// Runtime time : throw not_implemented_runtime_error
 template
 <
+    typename Tag,
     typename Term1,
     typename Term2,
     typename Term3
 >
 struct not_implemented_error
 {
+    /// Use not_implemented_policy to decide whether static assert or not
+    BOOST_MPL_ASSERT_MSG
+        (
+            not_implemented_policy<Tag>::value,
+            THIS_OPERATION_IS_NOT_OR_NOT_YET_IMPLEMENTED,
+            (
+                types<Term1, Term2, Term3>
+            )
+        );
 
-#ifndef BOOST_GEOMETRY_IMPLEMENTATION_STATUS_BUILD
-# define BOOST_GEOMETRY_IMPLEMENTATION_STATUS_BUILD false
-#endif
+    //TODO add support for compilers that do not support variadic template
+    template <typename... Args>
+    static not_implemented_result apply(Args...)
+    {
+        std::string what(Tag::what());
+        what += " is not implemented for the given parameters";
+        throw not_implemented_runtime_error(what);
+    }
+};
 
+/// Legacy not_implemented_error
+template
+<
+    typename Term1,
+    typename Term2,
+    typename Term3
+>
+struct not_implemented_error<void, Term1, Term2, Term3>
+{
     BOOST_MPL_ASSERT_MSG
         (
             BOOST_GEOMETRY_IMPLEMENTATION_STATUS_BUILD,
@@ -74,6 +192,13 @@ struct not_implemented_error
                 types<Term1, Term2, Term3>
             )
         );
+
+    //TODO add support for compilers that do not support variadic template
+    template <typename... Args>
+    static not_implemented_result apply(Args...)
+    {
+        throw not_implemented_runtime_error();
+    }
 };
 
 template <typename Tag>
@@ -96,36 +221,60 @@ template <> struct tag_to_term<geometry_collection_tag>     { typedef info::GEOM
 template <int D> struct tag_to_term<boost::mpl::int_<D> >   { typedef info::DIMENSION<D> type; };
 
 
-}
+} // namespace nyi
 
 
 template
 <
     typename Term1 = void,
     typename Term2 = void,
-    typename Term3 = void
+    typename Term3 = void,
+    typename Term4 = void
 >
 struct not_implemented
     : nyi::not_implemented_tag,
-      nyi::not_implemented_error
+      /// For backward compability
+      /// Switch between the old and the new not_implemented_error implementation
+      /// depend on the first template parameter (whether is an algorithm tag or not)
+      boost::mpl::if_
       <
-          typename boost::mpl::identity
-              <
-                  typename nyi::tag_to_term<Term1>::type
-              >::type,
-          typename boost::mpl::identity
-              <
-                  typename nyi::tag_to_term<Term2>::type
-              >::type,
-          typename boost::mpl::identity
-              <
-                  typename nyi::tag_to_term<Term3>::type
-              >::type
-      >
+        boost::is_base_of<algorithm_tag, Term1>,
+        nyi::not_implemented_error
+        <
+            Term1,
+            typename boost::mpl::identity<typename nyi::tag_to_term<Term2>::type>::type,
+            typename boost::mpl::identity<typename nyi::tag_to_term<Term3>::type>::type,
+            typename boost::mpl::identity<typename nyi::tag_to_term<Term4>::type>::type
+        >,
+        nyi::not_implemented_error
+        <
+            void,
+            typename boost::mpl::identity<typename nyi::tag_to_term<Term1>::type>::type,
+            typename boost::mpl::identity<typename nyi::tag_to_term<Term2>::type>::type,
+            typename boost::mpl::identity<typename nyi::tag_to_term<Term3>::type>::type
+        >
+      >::type
 {};
 
 
 }} // namespace boost::geometry
+
+
+/// Register an algorithm (i.e. define an algorithm tag)
+#define BOOST_GEOMETRY_REGISTER_ALGORITHM(Algo) \
+namespace boost { namespace geometry { \
+    struct Algo##_tag : algorithm_tag{ \
+        static const char* what() { return #Algo; } \
+    }; \
+}}
+
+/// Enable not implemented runtime errors for the given algorithm
+#define BOOST_GEOMETRY_NYI_RUNTIME_ERROR(Tag) \
+namespace boost { namespace geometry { \
+    template<> \
+    struct boost::geometry::not_implemented_policy<Tag> \
+        : boost::mpl::true_{}; \
+}}
 
 
 #endif // BOOST_GEOMETRY_ALGORITHMS_NOT_IMPLEMENTED_HPP
